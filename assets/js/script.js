e39c463fa61aac83da558e88c6bdefd20755d29e
Atsusan <as2k.sugimoto@gmail.com>
2024-03-16 00:40:19 +0900

    [fix] js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,50 +8,56 @@ jQuery(function () {
   });
 });
 
-// ハンバーガーメニュー
-
-// 家具
+// 左側のメニューボタンがクリックされたときの処理
 jQuery("#js-openbtn-left").click(function () {
-  jQuery("#js-openbtn-left").toggleClass("is-active");
-  jQuery("#js-g-nav").toggleClass("is-open");
-  jQuery("#js-body, #js-hamLeft").toggleClass("is-active");
+  toggleClassesLeft();
+  resetClassesRight();
 });
-jQuery("#js-hamLeft a").click(function () {
-  jQuery("#js-openbtn-left").toggleClass("is-active");
-  jQuery("#js-g-nav").toggleClass("is-open");
-  jQuery("#js-body, #js-hamLeft").toggleClass("is-active");
+
+// 左側のメニュー内のリンクがクリックされたときの処理
+jQuery("#js-hamLeft-inner a").click(function () {
+  resetClasses();
 });
+
+// メニュー部分がクリックされたときの処理
 jQuery("#js-g-nav").click(function () {
+  resetClasses();
+});
+
+// 右側のメニューボタンがクリックされたときの処理
+jQuery("#js-openbtn-right").click(function () {
+  toggleClassesRight();
+  resetClassesLeft();
+});
+
+function toggleClassesLeft() {
   jQuery("#js-openbtn-left").toggleClass("is-active");
   jQuery("#js-g-nav").toggleClass("is-open");
-  jQuery("#js-body, #js-hamLeft").toggleClass("is-active");
-});
-jQuery("#js-openbtn-left").click(function () {
-  jQuery("#js-hamRight").removeClass("is-active");
-});
-// jQuery("#js-openbtn-right").click(function () {
-//   jQuery("#js-openbtn-left").removeClass("is-active");
-//   jQuery("#js-g-nav").removeClass("is-open");
-//   jQuery("#js-body, #js-hamLeft").removeClass("is-active");
-// });
+  jQuery("#js-body").toggleClass("is-active");
+  jQuery("#js-body, #js-ham-left").removeClass("js-noneLeft");
+  jQuery("#js-ham-right").addClass("js-noneRight");
+}
 
-// 家電
-// jQuery("#js-openbtn-right").click(function () {
-//   jQuery("#js-openbtn-right").toggleClass("is-active");
-//   jQuery("#js-g-nav").toggleClass("is-open");
-//   jQuery("#js-body, #js-hamRight").toggleClass("is-active");
-// });
-// jQuery("#js-hamRight a").click(function () {
-//   jQuery("#js-openbtn-right").toggleClass("is-active");
-//   jQuery("#js-g-nav").toggleClass("is-open");
-//   jQuery("#js-body, #js-hamRight").toggleClass("is-active");
-// });
-// jQuery("#js-g-nav").click(function () {
-//   jQuery("#js-openbtn-right").toggleClass("is-active");
-//   jQuery("#js-g-nav").toggleClass("is-open");
-//   jQuery("#js-body, #js-hamRight").toggleClass("is-active");
-//   jQuery("#js-hamRight").toggleClass("is-active");
-// });
-// jQuery("#js-openbtn-right").click(function () {
-//   jQuery("#js-hamLeft").removeClass("is-active");
-// });
+function toggleClassesRight() {
+  jQuery("#js-openbtn-right").toggleClass("is-active");
+  jQuery("#js-g-nav").toggleClass("is-open");
+  jQuery("#js-body").toggleClass("is-active");
+  jQuery("#js-body, #js-ham-right").removeClass("js-noneRight");
+  jQuery("#js-ham-left").addClass("js-noneLeft");
+}
+
+function resetClasses() {
+  jQuery("#js-openbtn-left, #js-openbtn-right").removeClass("is-active");
+  jQuery("#js-g-nav").removeClass("is-open");
+  jQuery("#js-body, #js-ham-left, #js-ham-right").removeClass("is-active");
+}
+
+function resetClassesLeft() {
+  jQuery("#js-openbtn-left").removeClass("is-active");
+  jQuery("#js-body, #js-ham-left").removeClass("is-active");
+}
+
+function resetClassesRight() {
+  jQuery("#js-openbtn-right").removeClass("is-active");
+  jQuery("#js-body, #js-ham-right").removeClass("is-active");
+}
